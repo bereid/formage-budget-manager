12e573e74309ce758a588645f87e3760e0907fb5
somakanyasi <kanyasi.soma@gmail.com>
2019-01-24 13:26:19 +0100

    MongoClient imported

--- server/server.js
+++ server/server.js
@@ -4,16 +4,39 @@ const PORT = 4444;
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const cors = require("cors");
+const password = require("./password");
 const mockdb = require("../src/mockdb");
 const mongoose = require("mongoose");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const db = mongoose.connection;
+const dbRoute = `mongodb://fromageBoyz:${password}@ds131721.mlab.com:31721/fromage-db`;
+
 app.get("/api/budget", (req, res) => {
   res.send(mockdb);
 });
+
+app.get("/test", (req, res) => {
+  MongoClient.connect(
+    dbRoute,
+    { useNewUrlParser: true },
+    (err, database) => {
+      let collection = db.collection("budget");
+      collection.find({}).toArray((err, data) => {
+        return res.json(data);
+      });
+    }
+  );
+});
+
+db.once("open", () => console.log("Connected to the database"));
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on the PORT: ${PORT}`);
