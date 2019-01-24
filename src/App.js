c0f33da411404c4578b0110fbd53328b3fa94435
bereid <berei.daniel@gmail.com>
2019-01-24 14:14:24 +0100

    bootstrap added, flex grid added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.css';
 import DoughnutChartDemo from './components/Donut';
+import ScrollWidget from './components/ScrollWidget';
+import { Row, Col } from 'reactstrap';
+
+
 
 const App = ({ props }) => (
-  <DoughnutChartDemo budget={props}></DoughnutChartDemo>
+  <div>
+    <Row>
+          <Col xs="6" sm="2"><ScrollWidget /></Col>
+          <Col xs="6" sm="6"><DoughnutChartDemo budget={props} /></Col>
+          <Col sm="4"><ScrollWidget /></Col>
+        </Row>
+    
+    
+  </div>
 )
 
 export default App;
---
+++ src/components/ScrollWidget.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ScrollPanel } from 'primereact/scrollpanel';
+
+const ScrollWidget = () => (
+  <ScrollPanel style={{ width: '100%', height: '200px', backgroundColor: 'red' }}>
+  </ScrollPanel >
+);
+
+export default ScrollWidget;
