791a0c991531d57b163919edaed21018f3de1955
bereid <berei.daniel@gmail.com>
2019-02-17 16:35:41 +0100

    right widget scrolls

--- src/components/ScrollWidget.js
+++ src/components/ScrollWidget.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { ScrollPanel } from 'primereact/scrollpanel';
 import Switcher from './Switcher';
 
+const styles = {
+  scrollpanel: {
+    width: '25%',
+    height: '740px',
+    padding: '1em',
+    top: '60px',
+    overflow: 'scroll',
+  }
+}
+
 const ScrollWidget = ({ budget }) => (
-  <ScrollPanel style={{ width: '25%', height: '100vh', padding: '1em', top: '60px' }}>
+  <div style={styles.scrollpanel}>
     <Switcher props={budget} />
-  </ScrollPanel >
+  </div>
 );
 
 export default ScrollWidget;
